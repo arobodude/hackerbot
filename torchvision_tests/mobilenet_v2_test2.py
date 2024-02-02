bd93476d53af27cdce011cb2b8825342794123de
Ian <>
2024-02-02 15:26:51 -0700

    Added a couple of working torchvision examples

---
+++ torchvision_tests/mobilenet_v2_test2.py
@@ -0,0 +1,71 @@
+import os
+import ast
+import time
+
+import torch
+import numpy as np
+from torchvision import models, transforms
+
+import cv2
+from PIL import Image
+#import requests
+
+torch.backends.quantized.engine = 'qnnpack'
+
+cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 224)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 224)
+cap.set(cv2.CAP_PROP_FPS, 36)
+
+preprocess = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+net = models.quantization.mobilenet_v3_large(pretrained=True, quantize=True)
+# jit model to take it from ~20fps to ~30fps
+net = torch.jit.script(net)
+
+started = time.time()
+last_logged = time.time()
+frame_count = 0
+
+with open('imagenet1000_clsidx_to_labels.txt') as f:
+    classes = ast.literal_eval(f.read()) 
+
+#classes = ast.literal_eval(requests.get("https://gist.githubusercontent.com/yrevar/942d3a0ac09ec9e5eb3a/raw/238f720ff059c1f82f368259d1ca4ffa5dd8f9f5/imagenet1000_clsidx_to_labels.txt").content.decode('utf8'))
+
+with torch.no_grad():
+    while True:
+        # read frame
+        ret, image = cap.read()
+        if not ret:
+            raise RuntimeError("failed to read frame")
+
+        # convert opencv output from BGR to RGB
+        image = image[:, :, [2, 1, 0]]
+        permuted = image
+
+        # preprocess
+        input_tensor = preprocess(image)
+
+        # create a mini-batch as expected by the model
+        input_batch = input_tensor.unsqueeze(0)
+
+        # run model
+        output = net(input_batch)
+        # do something with output ...
+
+        # log model performance
+        frame_count += 1
+        now = time.time()
+        if now - last_logged > 1:
+            print(f"{frame_count / (now-last_logged)} fps")
+            last_logged = now
+            frame_count = 0
+
+        top = list(enumerate(output[0].softmax(dim=0)))
+        top.sort(key=lambda x: x[1], reverse=True)
+        os.system('clear')
+        for idx, val in top[:10]:
+            print(f"{val.item()*100:.2f}% {classes[idx]}")
