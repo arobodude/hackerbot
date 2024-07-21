a9500d36f726aa7284e1c18541282db14fd9972f
Skylar Castator <aerialchemist@gmail.com>
2024-07-20 19:17:09 -0600

    fixed issues with http request

--- Server/hackerbot1/views.py
+++ Server/hackerbot1/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 import hackerbot1.hackerbot_commands as hb
 import sys
+from django.http import JsonResponse
 
 def print_command(request):
     if request.method == 'POST':
         input_text = request.POST.get('input_text')
         print("Button was clicked!", file=sys.stdout)
         hb.send_command(input_text)
-    else:
-        print("No message sent")
+        return JsonResponse({'status': 'success', 'message': 'Command executed.'})
+    return JsonResponse({'status': 'failure', 'message': 'Invalid request.'})
 
 
 
