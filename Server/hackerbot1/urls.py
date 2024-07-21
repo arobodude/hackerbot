aac51c451de4578eb81ddf86589bbec23aaa1d41
Skylar Castator <aerialchemist@gmail.com>
2024-07-20 20:38:06 -0600

    fixed issue with send_move_command

--- Server/hackerbot1/urls.py
+++ Server/hackerbot1/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
         path("", views.home, name="home"),
         path('send_command/', views.send_command, name='send_command'),
+        path('send_move_command/', views.send_move_command, name='send_move_command'),
         ]
