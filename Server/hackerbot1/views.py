8bb83c846868899f2e5bd082c7a38ad54a02e841
Skylar Castator <aerialchemist@gmail.com>
2024-07-20 19:43:51 -0600

    removing notifications from application

--- Server/hackerbot1/views.py
+++ Server/hackerbot1/views.py
@@ -9,11 +9,11 @@ def hackbot_command(request):
         input_text = request.POST.get('input_text')
         print("Button was clicked!", file=sys.stdout)
         hb.send_command(input_text)
-        
+
 def send_command(request):
     if request.method == 'POST':
         text = request.POST.get('text_input')
-        threading.Thread(target=hackbot_command, args=(text,)).start()
+        hackbot_command(text)
         return JsonResponse({'status': 'Processing started'})
     return JsonResponse({'status': 'Invalid request'}, status=400)
 
